1e626654cfe6d32726e728a6fe7506564098e3d2
Eustus Mwirigi <mwirigieustus97@gmail.com>
2024-04-09 16:00:20 +0300

    Change @locale decorators

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-'''Task 0: Basic Flask app
-'''
+"""Task 0: Basic Flask app
+"""
 
 from flask import Flask, render_template
 
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    '''default route'''
+    """default route"""
     return render_template('0-index.html')
 
 
--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-'''get locale from request
-'''
+"""get locale from request
+"""
 
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config:
-    '''Config class'''
+    """Config class"""
 
     DEBUG = True
     LANGUAGES = ["en", "fr"]
@@ -21,7 +21,6 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@babel.localeselector
 def get_locale() -> str:
     """Retrieves the locale for a web page.
 
@@ -31,13 +30,16 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+babel.init_app(app, locale_selector=get_locale)
+
+
 @app.route('/')
 def index() -> str:
-    '''default route
+    """default route
 
     Returns:
         html: homepage
-    '''
+    """
     return render_template("3-index.html")
 
 
